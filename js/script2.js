ec6507bdd20a172f30ad46dc49d12112325ca6f5
[jamirgvtimes] <[muhumuzajamir2016@gmail.com]>
2022-01-06 23:04:51 +0300

    Second commit of day one

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,16 @@
+
+// code for possing the top navbar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -8,24 +21,3 @@ window.onclick = function(event) {
     }
 }
 
-//Code for the slider image 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-// end of the slider image
